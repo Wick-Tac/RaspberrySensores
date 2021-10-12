d161649b554e327db7903648e922da98a9cdd78e
Wick-Tac <ghostrekoon@gmail.com>
2021-10-12 17:13:40 -0500

    Correción del codigo

---
+++ Temp_Pi.py
@@ -0,0 +1,15 @@
+import Adafruit_DHT  
+import time  
+
+while True:
+  sensor = Adafruit_DHT.DHT11 
+  pin = 4
+  humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
+
+  print ('Humedad: ' , humedad)
+  print ('Temperatura: ' , temperatura)
+ 
+  time.sleep(1)
+
+
+
